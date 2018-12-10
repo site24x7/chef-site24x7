16b471a6cfb0bae80c6aa151e1b63df02bcd7f37
fr33l <andrii.zakharkiv@synthasite.com>
2018-12-10 16:34:49 +0200

    oops, missplacement

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,8 @@ default['Site24x7']['devicekey'] = 'YOUR_USER_DEVICE_KEY'
 default['Site24x7']['proxy'] = ''
 
 # package identification
-if node['kernel']['machine'] == 'i686'
+if node['kernel']['machine'] == 'i386'
   default['package'] = 'Site24x7_Linux_32bit.install'
-elsif node['kernel']['machine'] == 'i386'
+elsif node['kernel']['machine'] == 'i686'
   default['package'] = 'Site24x7_Linux_64bit.install'
 end
