d3cfa4e80b3d6ed9a8cc57f6f2f78e046f22378f
fr33l <andrii.zakharkiv@synthasite.com>
2018-12-10 16:20:12 +0200

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'support@site24x7.com'
 license          'All rights reserved'
 description      'Installs/Configures site24x7 server monitoring agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
