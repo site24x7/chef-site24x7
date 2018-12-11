c83d919bd5cf06ee7cfa0dbe85f0cde7bcfb606c
fr33l <andrii.zakharkiv@synthasite.com>
2018-12-11 14:22:49 +0200

    oops, i686 !=64

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,9 +2,10 @@
 default['Site24x7']['devicekey'] = 'YOUR_USER_DEVICE_KEY'
 default['Site24x7']['proxy'] = ''
 
+
 # package identification
-if node['kernel']['machine'] == 'i386'
+if node['kernel']['machine'] == 'i386' || node['kernel']['machine'] == 'i686'
   default['package'] = 'Site24x7_Linux_32bit.install'
-elsif node['kernel']['machine'] == 'i686'
+else
   default['package'] = 'Site24x7_Linux_64bit.install'
 end
